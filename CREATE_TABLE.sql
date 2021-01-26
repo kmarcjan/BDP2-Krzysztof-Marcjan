584b602566330e108e38f5e821722e064df47132
Krzysztof Marcjan <krzysztof.marcjan@gmail.com>
2021-01-26 02:22:58 +0100

    Added excercise 7 files

---
+++ CREATE_TABLE.sql
@@ -0,0 +1,11 @@
+CREATE TABLE CSV_Customers (
+    EmailAddress nvarchar(255),
+    LastName nvarchar(255),
+    FirstName nvarchar(255),
+    Address nvarchar(255),
+    City nvarchar(255),
+	PhoneNumber nvarchar(255),
+    CREATE_TIMESTAMP datetime,
+	UPDATE_TIMESTAMP datetime,
+
+);
